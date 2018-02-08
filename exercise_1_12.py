3d8a0cc9433004a867658cc0194a6263f2f7183b
Sasha Sommerfeldt <somme153@umn.edu>
2018-02-07 22:40:37 -0600

    got squares in right places on screen

--- exercise_1_12.py
+++ exercise_1_12.py
@@ -8,8 +8,8 @@ from psychopy import visual,event,core
 # 5. Show a red square for 1 s then change its orientation by 45-deg
 # To change the orientation by a certain degree-value use square.setOri(value) where `value` is the new orientation.
 win = visual.Window([400,400],color="black", units='pix')
-square = visual.Rect(win,lineColor="black",fillColor="red",size=[100,100])
-squareB = visual.Rect(win,lineColor="black",fillColor="blue",size=[100,100])
+square = visual.Rect(win,lineColor="black",fillColor="red",size=[100,100], pos=[-75,0])
+squareB = visual.Rect(win,lineColor="black",fillColor="blue",size=[100,100], pos=[75,0])
 
 
 
@@ -20,7 +20,6 @@ while x > 1 :
 	while i < 361 :
 		square.ori += 1
 		square.draw()
-		win.flip()
 		core.wait(1/360)
 		i +=1
 
