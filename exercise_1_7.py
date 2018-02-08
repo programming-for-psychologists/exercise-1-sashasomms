339eab4affab7e06d76172cc20a60fadb9f5c449
Sasha Sommerfeldt <somme153@umn.edu>
2018-02-08 09:03:37 -0600

    updates from class

--- exercise_1_7.py
+++ exercise_1_7.py
@@ -9,7 +9,22 @@ from psychopy import visual,event,core
 win = visual.Window([400,400],color="black", units='pix')
 square = visual.Rect(win,lineColor="black",fillColor="red",size=[100,100])
 
+inc = 45
+rot = 360
+perTime = 1.0
+win.flip()
+square.draw()
+win.flip()
+for num in range (rot/inc):
+    core.wait(perTime/(rot/inc))
+    win.flip()
+    square.ori += inc
+    square.draw()
+    win.flip()
+sys.exit()
 
+
+'''
 x = 1
 while x < 4: 
 	i = 0
@@ -20,7 +35,7 @@ while x < 4:
 		core.wait(1/360)
 		i +=1
 
-
+'''
 
 
 sys.exit()
